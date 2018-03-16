a6d7a3314bdf512d6922b5444ffb355d8a162ef2
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-03-16 17:56:32 -0300

    changed id for testing

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stories:app --log-file=-
---
+++ stories.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import json
+
+app = Flask(__name__)
+
+@app.route('/id/<name>')
+def get_id_by_name(name):
+    data = {'id': 'someId', 'name': name}
+    response = app.response_class(
+        status=200,
+        mimetype='application/json',
+        response=json.dumps(data)
+    )
+    return response
+
+if __name__ == '__main__':
+    app.run()
