1e3aa03936df96e2888aa04ea205736ccd857c46
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-03-30 05:29:58 -0300

    lint

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import json
 
-app = Flask(__name__)
+"""
+Main module
+"""
+
+APP = Flask(__name__)
 
 
-@app.route('/')
+@APP.route('/')
 def get_id_by_name():
+    """Testing method"""
     data = {'id': 'someOtherId', 'name': 'rodrigo.de.rosa'}
-    response = app.response_class(
+    response = APP.response_class(
         status=200,
         mimetype='application/json',
         response=json.dumps(data)
@@ -16,4 +21,4 @@ def get_id_by_name():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, threaded=True)
+    APP.run(host='0.0.0.0', port=8080, threaded=True)
