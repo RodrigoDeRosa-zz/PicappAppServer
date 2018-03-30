635972842ef8ff72c22ebe99532cf2efb14d09d2
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-03-30 05:42:46 -0300

    lint

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+"""Main testing module"""
 import os
 import sys
 import inspect
@@ -6,20 +7,19 @@ from flask import json
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
 from src import main
 
-"""Main testing module"""
 
-class HomeResponseTest(unittest.TestCase):
+class MainTest(unittest.TestCase):
     """Testing class"""
     def setUp(self):
         """Testing set up"""
-        self.app = main.app.test_client()
+        self.app = main.APP.test_client()
         self.app.testing = True
 
-    def test_home_page(self):
+    def test(self):
         """Main test"""
         home = self.app.get('/')
         self.assertEqual(json.loads(home.data)['id'], 'someOtherId')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
