ef6b17488a06c5f2b810c2754913958fbf95ebbd
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-03-30 02:23:26 -0300

    travis

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -1,5 +1,7 @@
 import unittest
 from flask import json
+import os, sys, inspect
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
 import stories
 
 
