209c9eea1ee03458e2a138f62a2144ecbe8fcf78
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-03-30 03:39:11 -0300

    travis

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_id_by_name():
-    data = {'id': 'someOtherId', 'name': 'rodrigo.de-rosa'}
+    data = {'id': 'someOtherId', 'name': 'rodrigo.de.rosa'}
     response = app.response_class(
         status=200,
         mimetype='application/json',
@@ -17,13 +17,3 @@ def get_id_by_name():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True)
-
-"""
-- stage: build docker image
-    script:
-    - docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-    - docker build -t picapp-app-server .
-    - docker images
-    - docker tag picapp-app-server $DOCKER_USERNAME/picapp-app-server
-    - docker push $DOCKER_USERNAME/picapp-app-server
-"""
