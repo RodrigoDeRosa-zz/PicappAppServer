61566f8e6c577a9bb700c9e5239bd285aa08a95d
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2018-04-03 13:58:55 -0300

    lint refactoring

--- src/resources/response_builder.py
+++ src/resources/response_builder.py
@@ -1,13 +1,14 @@
 from flask import jsonify
 from flask import make_response
 
+
 class ResponseBuilder:
-	@staticmethod
-	def build_response(response,status_code = 200):
-		return make_response(jsonify(response),status_code)
 
+    @staticmethod
+    def build_response(response, status_code=200):
+        return make_response(jsonify(response), status_code)
 
-	@staticmethod
-	def build_error_response(error_message,status_code):
-		response = {'message':error_message, 'status_code':status_code}
-		return ResponseBuilder.build_response(response,status_code)
+    @staticmethod
+    def build_error_response(error_message, status_code):
+        response = {'message': error_message, 'status_code': status_code}
+        return ResponseBuilder.build_response(response, status_code)
