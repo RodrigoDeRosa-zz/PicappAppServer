f80959b10190504d332bbb8c5c65df1c21f8ad00
Martin Errazquin <martin.errazquin@hotmail.com>
2018-04-04 17:03:42 -0300

    adding structure

---
+++ src/model/token.py
@@ -0,0 +1,40 @@
+from src.model.database import mongo
+from exceptions import Exception
+
+
+class InvalidTokenException(Exception):
+    pass
+
+
+class ExpiredTokenException(Exception):
+    pass
+
+
+class Token(object):
+
+    @staticmethod
+    def _get_tokens_db():
+        return mongo.db.tokens
+
+    @staticmethod
+    def validate(token):
+        """Receives a token and uses to identify a User, or raises an exception"""
+        # with mongo.db.tokens as Tokens, tk = Tokens.find_one({'token': token}
+        # if tk is None:
+        #   raise InvalidToken
+        # if somehow.get_epochs > tk['expiresAt']:
+        #   raise ExpiredTokenException
+        # return tk['username']
+        raise NotImplemented()
+
+    @staticmethod
+    def create(token,expiration_epochs,username):
+        """Saves to DB a newly created Token with expiration date (in epochs) associated
+        with a user"""
+        # with mongo.db.tokens as Tokens,
+        # tk = Tokens.find_one({'username':username})
+        # Tokens.insert({'token': token, 'expiresAt': expiration_epochs, 'username': username}
+
+        # if i wanted to force only one active session
+        # if tk is not None:
+        #   tk.find_and_delete({'_id':tk['_id']}
