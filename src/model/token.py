f49107e0e24392f02b832a67f90042186b915bdf
Martin Errazquin <martin.errazquin@hotmail.com>
2018-04-06 15:32:58 -0300

    Token done

--- src/model/token.py
+++ src/model/token.py
@@ -1,4 +1,5 @@
 from src.model.database import mongo
+import time
 
 
 class ExpiredTokenException(Exception):
@@ -12,25 +13,27 @@ class Token(object):
         return mongo.db.tokens
 
     @staticmethod
+    def _get_current_epochs():
+        return int(time.time())
+
+    @staticmethod
     def validate(token):
         """Receives a token and looks for it in the database, returning True or False whether it is found
         or not, or raising an exception if it expired"""
-        # with mongo.db.tokens as Tokens, tk = Tokens.find_one({'token': token}
         tk = Token._get_tokens_db().find_one({'token': token})
         if tk is None:
             return False
-        # if somehow.get_present_epochs() > tk['expiresAt']
-        #   raise ExpiredTokenException
+        if Token._get_current_epochs() > tk['expiresAt']:
+            raise ExpiredTokenException
         return True
 
     @staticmethod
     def create(token, expiration_epochs, username):
         """Saves to DB a newly created Token with expiration date (in epochs) associated
         with a user"""
-        # with mongo.db.tokens as Tokens,
-        # tk = Tokens.find_one({'username':username})
-        # Tokens.insert({'token': token, 'expiresAt': expiration_epochs, 'username': username}
-
-        # if i wanted to force only one active session
-        # if tk is not None:
-        #   tk.find_and_delete({'_id':tk['_id']}
+        tk = Token._get_tokens_db().find_one({'username': username})
+        new_token_id = Token._get_tokens_db().insert({'token': token, 'expiresAt': expiration_epochs, 'username': username})
+        # enforce only one active session
+        if tk is not None:
+            tk.find_and_delete({'_id': tk['_id']})
+        return new_token_id
