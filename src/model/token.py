45dbfbc2c6232188f347e777bab1c4149ef3ad3e
Martin Errazquin <martin.errazquin@hotmail.com>
2018-04-06 14:04:37 -0300

    need to finish methods

--- src/model/token.py
+++ src/model/token.py
@@ -13,17 +13,18 @@ class Token(object):
 
     @staticmethod
     def validate(token):
-        """Receives a token and uses to identify a User, or raises an exception"""
+        """Receives a token and looks for it in the database, returning True or False whether it is found
+        or not, or raising an exception if it expired"""
         # with mongo.db.tokens as Tokens, tk = Tokens.find_one({'token': token}
-        token['asd'] = 'asd' #just to shut up linter
-        # if tk is None:
-            # return False
+        tk = Token._get_tokens_db().find_one({'token': token})
+        if tk is None:
+            return False
         # if somehow.get_present_epochs() > tk['expiresAt']
         #   raise ExpiredTokenException
         return True
 
     @staticmethod
-    def create(token,expiration_epochs,username):
+    def create(token, expiration_epochs, username):
         """Saves to DB a newly created Token with expiration date (in epochs) associated
         with a user"""
         # with mongo.db.tokens as Tokens,
